721e95bfef5fe6c55b9f8e3616391fe8b148241a
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2019-08-27 16:28:26 -0300

    Conhecendo os verbos http

--- rest-with-spring-boot/src/main/java/br/com/wendt/restwithspringboot/controller/PersonController.java
+++ rest-with-spring-boot/src/main/java/br/com/wendt/restwithspringboot/controller/PersonController.java
@@ -4,10 +4,9 @@ import br.com.wendt.restwithspringboot.model.Person;
 import br.com.wendt.restwithspringboot.services.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/person")
@@ -16,10 +15,30 @@ public class PersonController {
     @Autowired
     private PersonService service;
 
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Person> findAll() {
+        return service.findAll();
+    }
+
     @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Person findById(@PathVariable(value = "id") String id) throws Exception {
+    public Person findById(@PathVariable(value = "id") String id) {
         return service.findById(id);
 
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Person create(@RequestBody Person person){
+        return service.create(person);
+    }
+
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Person update(@RequestBody Person person){
+        return service.update(person);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public void delete(@PathVariable(value = "id") String id){
+        service.delete(id);
     }
 
 }
--- rest-with-spring-boot/src/main/java/br/com/wendt/restwithspringboot/services/PersonService.java
+++ rest-with-spring-boot/src/main/java/br/com/wendt/restwithspringboot/services/PersonService.java
@@ -3,12 +3,26 @@ package br.com.wendt.restwithspringboot.services;
 import br.com.wendt.restwithspringboot.model.Person;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 @Service
 public class PersonService {
 
     private final AtomicLong counter = new AtomicLong();
+
+    public Person create(Person person){
+        return person;
+    }
+
+    public Person update(Person person){
+        return person;
+    }
+
+    public void delete(String id){
+
+    }
 
     public Person findById(String id) {
         Person person = new Person();
@@ -17,7 +31,25 @@ public class PersonService {
         person.setLastName("Wendt");
         person.setAddress("Sao paulo - Brasil");
         person.setGender("Male");
+        return person;
+    }
 
+    public List<Person> findAll(){
+        List<Person> persons = new ArrayList<Person>();
+        for (int i = 0; i < 8; i++){
+            Person person = mockPerson(i);
+            persons.add(person);
+        }
+        return persons;
+    }
+
+    private Person mockPerson(int i) {
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("Person name " + i);
+        person.setLastName("Last name " + i);
+        person.setAddress("Some address in Brazil " + i);
+        person.setGender("Male");
         return person;
     }
 }
