39eb7b1412c9e447e975f4a02a435fb576bea6e5
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2019-10-14 21:32:52 -0300

    Configurando migrations com Flyway

---
+++ rest-with-spring-boot/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,18 @@
+INSERT INTO `rest_with_springboot`.`person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+ ('1', 'Sao Paulo - SP', 'Luiz Felipe', 'Male', 'Wendt Campos'),
+ ('2', 'Some address in Brazil 0', 'Gabriela', 'Female', 'Naka'),
+ ('3', 'Some address in Brazil 0', 'Teste de geleia com vo', 'Male', 'Qualquer coisa'),
+ ('8', 'Some address in Brazil 0', 'Luiz Felipe', 'Male', 'Qualquer coisa'),
+ ('9', 'Some address in Brazil 0', 'Luiz Felipe com a v2', 'Male', 'Qualquer coisa');
+
+
+--INSERT INTO `rest_with_springboot`.`person` (`id`, `address`, `first_name`, `gender`, `last_name`)
+--VALUES ('1', 'Sao Paulo - SP', 'Luiz Felipe', 'Male', 'Wendt Campos');
+--INSERT INTO `rest_with_springboot`.`person` (`id`, `address`, `first_name`, `gender`, `last_name`)
+--VALUES ('2', 'Some address in Brazil 0', 'Gabriela', 'Female', 'Naka');
+--INSERT INTO `rest_with_springboot`.`person` (`id`, `address`, `first_name`, `gender`, `last_name`)
+--VALUES ('3', 'Some address in Brazil 0', 'Teste de geleia com vo', 'Male', 'Qualquer coisa');
+--INSERT INTO `rest_with_springboot`.`person` (`id`, `address`, `first_name`, `gender`, `last_name`)
+--VALUES ('8', 'Some address in Brazil 0', 'Luiz Felipe', 'Male', 'Qualquer coisa');
+--INSERT INTO `rest_with_springboot`.`person` (`id`, `address`, `first_name`, `gender`, `last_name`)
+--VALUES ('9', 'Some address in Brazil 0', 'Luiz Felipe com a v2', 'Male', 'Qualquer coisa');
