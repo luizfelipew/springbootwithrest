3e87eee0a187aa10ba199af134f260d5191885c7
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2020-05-29 12:27:32 -0300

    Implementando controller responsavel pela autenticacao

---
+++ rest-with-spring-boot/src/main/java/br/com/wendt/restwithspringboot/controller/AuthController.java
@@ -0,0 +1,69 @@
+package br.com.wendt.restwithspringboot.controller;
+
+import br.com.wendt.restwithspringboot.data.vo.v1.BookVO;
+import br.com.wendt.restwithspringboot.repository.UserRepository;
+import br.com.wendt.restwithspringboot.security.AccountCredentialsVO;
+import br.com.wendt.restwithspringboot.security.jwt.JwtTokenProvider;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+import static org.springframework.http.ResponseEntity.*;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @ApiOperation(value = "Authenticate a user by credentials")
+    @PostMapping(value = "/signin", produces = {"application/json", "application/xml", "application/x-yaml"},
+        consumes = {"application/json", "application/xml", "application/x-yaml"})
+    public ResponseEntity signin(@RequestBody AccountCredentialsVO data) {
+        try{
+            var username = data.getUsername();
+            var password = data.getPassword();
+
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+
+            var user = userRepository.findByUsername(username);
+
+            var token = "";
+
+            if (Objects.nonNull(user)) {
+                token = jwtTokenProvider.createToken(username, user.getRoles());
+
+            } else {
+                throw new UsernameNotFoundException("Username" + username + " not found!");
+            }
+
+            Map<Object, Object> model = new HashMap<>();
+            model.put("username", username);
+            model.put("token", token);
+
+            return ok(model);
+        } catch (AuthenticationException ex) {
+            throw new BadCredentialsException("Invalid username/password supplied");
+        }
+    }
+}
