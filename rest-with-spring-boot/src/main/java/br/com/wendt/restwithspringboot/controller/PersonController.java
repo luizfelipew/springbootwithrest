c9f51ae89cc85bc46400578841f0920fbc556aa6
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2020-06-09 17:15:06 -0300

    removendo os warnings por causa do assembler

--- rest-with-spring-boot/src/main/java/br/com/wendt/restwithspringboot/controller/PersonController.java
+++ rest-with-spring-boot/src/main/java/br/com/wendt/restwithspringboot/controller/PersonController.java
@@ -12,6 +12,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PagedResourcesAssembler;
 import org.springframework.hateoas.PagedResources;
+import org.springframework.hateoas.Resource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -39,12 +40,14 @@ public class PersonController {
     @Autowired
     private PersonService service;
 
+    @Autowired
+    private PagedResourcesAssembler<PersonVO> assembler;
+
     @ApiOperation(value = "Find all people recorded")
     @GetMapping(produces = {"application/json", "application/xml", "application/x-yaml"})
-    public ResponseEntity<PagedResources<PersonVO>> findAll(@RequestParam(value = "page", defaultValue = "0") final int page,
+    public ResponseEntity<?> findAll(@RequestParam(value = "page", defaultValue = "0") final int page,
         @RequestParam(value = "limit", defaultValue = "12") final int limit,
-        @RequestParam(value = "direction", defaultValue = "asc") final String direction,
-        final PagedResourcesAssembler assembler) {
+        @RequestParam(value = "direction", defaultValue = "asc") final String direction) {
 
         var sortDirection = "desc".equalsIgnoreCase(direction) ? Sort.Direction.DESC : Sort.Direction.ASC;
 
@@ -55,16 +58,17 @@ public class PersonController {
             .forEach(p -> p.add(
                 linkTo(methodOn(PersonController.class).findById(p.getKey())).withSelfRel())
                     );
-        return new ResponseEntity<>(assembler.toResource(persons), HttpStatus.OK);
+
+        final PagedResources<?> resources = assembler.toResource(persons);
+        return new ResponseEntity<>(resources, HttpStatus.OK);
     }
 
     @ApiOperation(value = "Find all people recorded")
     @GetMapping(value = "/findPersonByName/{firstName}", produces = {"application/json", "application/xml", "application/x-yaml"})
-    public ResponseEntity<PagedResources<PersonVO>> findPersonByName(@PathVariable("firstName") final String firstName,
+    public ResponseEntity<?> findPersonByName(@PathVariable("firstName") final String firstName,
         @RequestParam(value = "page", defaultValue = "0") final int page,
         @RequestParam(value = "limit", defaultValue = "12") final int limit,
-        @RequestParam(value = "direction", defaultValue = "asc") final String direction,
-        final PagedResourcesAssembler assembler) {
+        @RequestParam(value = "direction", defaultValue = "asc") final String direction) {
 
         var sortDirection = "desc".equalsIgnoreCase(direction) ? Sort.Direction.DESC : Sort.Direction.ASC;
 
@@ -75,7 +79,8 @@ public class PersonController {
             .forEach(p -> p.add(
                 linkTo(methodOn(PersonController.class).findById(p.getKey())).withSelfRel())
                     );
-        return new ResponseEntity<>(assembler.toResource(persons), HttpStatus.OK);
+        final PagedResources<?> resources = assembler.toResource(persons);
+        return new ResponseEntity<>(resources, HttpStatus.OK);
     }
 
     //    @CrossOrigin(origins = {"http://localhost:8080", "http://xablausites.com"})
