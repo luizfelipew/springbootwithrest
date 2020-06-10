ec649fafa99b0e59d9c4d88feda25ca8a03637da
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2020-06-10 14:08:51 -0300

    criando o servico para armazenamento do arquivo em disco

---
+++ rest-with-spring-boot/src/main/java/br/com/wendt/restwithspringboot/services/FileStorageService.java
@@ -0,0 +1,48 @@
+package br.com.wendt.restwithspringboot.services;
+
+import br.com.wendt.restwithspringboot.config.FileStorageConfig;
+import br.com.wendt.restwithspringboot.exception.FileStorageException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+@Service
+public class FileStorageService {
+
+    private final Path fileStorageLocation;
+
+    @Autowired
+    public FileStorageService(FileStorageConfig fileStorageConfig) {
+        this.fileStorageLocation = Paths.get(fileStorageConfig.getUploadDir())
+            .toAbsolutePath().normalize();
+        try {
+            Files.createDirectories(this.fileStorageLocation);
+        } catch (Exception ex) {
+            throw new FileStorageException("Could not create the directory where the uploaded files will be stored", ex);
+        }
+    }
+
+    public String storedFiles(final MultipartFile file) {
+        final String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+
+        try {
+            if (fileName.contains("..")) {
+                throw new FileStorageException("Sorry! Filename contains invalid path sequence " + fileName);
+            }
+
+            Path targetLocation = this.fileStorageLocation.resolve(fileName);
+            Files.copy(file.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
+
+            return fileName;
+        } catch (Exception ex) {
+            throw new FileStorageException("Could not store file " + fileName + ". Please try again!", ex);
+        }
+    }
+
+}
